6510024f9da9c508329bb0a6f834d6e599ae3a6e
Perfect <inve15065@gmail.com>
2026-02-19 11:17:31 +0300

    Fix CORS

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 // frontend/src/config.js
-const API_BASE_URL = "https://your-backend-domain.com"; // <-- deployed backend URL
+const API_BASE_URL = "https://pteach-backend.onrender.com"; // <-- deployed backend URL
 export default API_BASE_URL;
